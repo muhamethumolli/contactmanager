ef83a622bf87188501e158473458fc2bb97194d5
mh38924 ubt-uni.net <mh38924@ubt-uni.net>
2019-05-07 14:44:57 +0200

    Updates

---
+++ precache-manifest.b7802f2a91e6e4b750bdfdfbacc30ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "da67cb4b5d6618600eab",
+    "url": "/contactmanager/static/js/main.152459e6.chunk.js"
+  },
+  {
+    "revision": "53b956163519fb00b024",
+    "url": "/contactmanager/static/js/2.a46c82bc.chunk.js"
+  },
+  {
+    "revision": "da67cb4b5d6618600eab",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "53b956163519fb00b024",
+    "url": "/contactmanager/static/css/2.63a84eed.chunk.css"
+  },
+  {
+    "revision": "cedb7fbb34e85c7a0438012a5653c6f1",
+    "url": "/contactmanager/index.html"
+  }
+];
